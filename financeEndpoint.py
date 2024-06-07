77f397a7323126301f5e71022ba9ea3325008900
kadek dika <dika.rama@undiksha.ac.id>
2024-06-07 16:12:02 +0800

    endpoint api klasifikasi

--- financeEndpoint.py
+++ financeEndpoint.py
@@ -1,45 +1,60 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, request
 import numpy as np
 import joblib
 import tensorflow as tf
 
 app = Flask(__name__)
 
-# Muat model dan scaler
+# Load model and scaler
 model_path = 'best_model.h5'
 scaler_path = 'scaler.pkl'
 model = tf.keras.models.load_model(model_path)
 scaler = joblib.load(scaler_path)
 
 # Label mapping
-INDEX2LABEL = {0: 'kurang', 1: 'baik', 2: 'sangat baik'}
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+INDEX2LABEL = {0: 'Kurang Baik', 1: 'Baik', 2: 'Sangat Baik'}
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        # Ambil input dari form
-        penghasilan_bulanan = float(request.form['penghasilan_bulanan'])
-        pengeluaran_bulanan = float(request.form['pengeluaran_bulanan'])
-        tabungan_bulanan = float(request.form['tabungan_bulanan'])
+    if request.is_json:
+        try:
+            # Get input from JSON
+            data = request.get_json()
+            penghasilan_bulanan = float(data['penghasilan_bulanan'])/1000000
+            pengeluaran_bulanan = float(data['pengeluaran_bulanan'])/1000000
+            tabungan_bulanan = float(data['tabungan_bulanan'])/1000000
 
-        # Lakukan prediksi
-        new_input = [penghasilan_bulanan, pengeluaran_bulanan, tabungan_bulanan]
-        predicted_label = predict_with_new_input(new_input)
+            # Perform prediction
+            new_input = [penghasilan_bulanan, pengeluaran_bulanan, tabungan_bulanan]
+            predicted_label = predict_with_new_input(new_input)
 
-        return render_template('result.html', predicted_label=predicted_label)
+            return jsonify({'predicted_label': predicted_label, 'dd':pengeluaran_bulanan})
+        except (KeyError, TypeError, ValueError) as e:
+            return jsonify({'error': str(e)}), 400
+    elif request.form:
+        try:
+            # Get input from form-data
+            penghasilan_bulanan = float(request.form['penghasilan_bulanan'])/1000000
+            pengeluaran_bulanan = float(request.form['pengeluaran_bulanan'])/1000000
+            tabungan_bulanan = float(request.form['tabungan_bulanan'])/1000000
+
+            # Perform prediction
+            new_input = [penghasilan_bulanan, pengeluaran_bulanan, tabungan_bulanan]
+            predicted_label = predict_with_new_input(new_input)
+
+            return jsonify({'predicted_label': predicted_label, 'dd':pengeluaran_bulanan})
+        except (KeyError, TypeError, ValueError) as e:
+            return jsonify({'error': str(e)}), 400
+        
 
 def predict_with_new_input(new_input):
-    # Standarisasi input baru
+    # Standardize new input
     new_input_scaled = scaler.transform([new_input])
 
-    # Prediksi
+    # Predict
     prediction = np.argmax(model.predict(new_input_scaled), axis=-1)
 
-    # Konversi prediksi menjadi label asli
+    # Convert prediction to original label
     predicted_label = INDEX2LABEL[prediction[0]]
     return predicted_label
 
